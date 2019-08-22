a034da26c9bd4a021de30cfb74a7798dc5d3c4d4
ZUJ0007 <zuj0007@m-c02w51lcg8wl.jet.local>
2019-08-22 13:57:48 -0400

    remove interceptors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,8 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount () {
@@ -35,6 +36,8 @@ class BurgerBuilder extends Component {
         axios.get(CONFIG.fireBaseUrl + 'ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data})
+        }).catch(error => {
+            this.setState({ error: true})
         })
     }
 
@@ -122,7 +125,7 @@ class BurgerBuilder extends Component {
         }
         
         let orderSummary = null;
-        let burger = <Spinner />
+        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
         if (this.state.ingredients) {
             
             burger = (
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,14 +8,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
 
-        componentDidMount () {
-            axios.interceptors.request.use(req => {
+
+        componentWillMount() {
+            this.requestInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            console.log("will unmont", this.requestInterceptor, this.responseInterceptor)
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.request.eject(this.responseInterceptor);
+
         }
         errorConfirmedHandler = () => {
             this.setState({error: null})
